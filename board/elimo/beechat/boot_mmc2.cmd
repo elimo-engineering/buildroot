bae64b4ca01da068a4c00883b493ded03c5348d5
Matteo Scordino <matteo.scordino@gmail.com>
2021-07-21 22:06:38 +0100

    fix(beechat): wrong dts in boot for mmc2

--- board/elimo/beechat/boot_mmc2.cmd
+++ board/elimo/beechat/boot_mmc2.cmd
@@ -3,8 +3,8 @@ setenv bootargs console=ttyS0,115200 panic=5 console=tty0 rootwait root=/dev/mmc
 # Example kernel cmdline to enable:
 # - brcmfmac debugging
 # - dyndbg for sdio_io.c (dyndbg must be enabled in the kernel config)
-#setenv bootargs console=ttyS0,115200 panic=5 console=tty0 rootwait root=/dev/mmcblk0p2 earlyprintk rw brcmfmac.debug=0x00100006 ignore_loglevel dyndbg='\"file sdio_io.c +pfl\"'
+#setenv bootargs console=ttyS0,115200 panic=5 console=tty0 rootwait root=/dev/mmcblk2p2 earlyprintk rw brcmfmac.debug=0x00100006 ignore_loglevel dyndbg='\"file sdio_io.c +pfl\"'
 
 load mmc 2:1 0x41000000 zImage
-load mmc 2:1 0x41800000 sun8i-s3-elimo-initium.dtb
+load mmc 2:1 0x41800000 sun8i-s3-elimo-beechat.dtb
 bootz 0x41000000 - 0x41800000
